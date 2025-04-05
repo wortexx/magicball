fefa9cb664a4c9ccccf3622911ca4f45363e283f
Viktor Stepanenko <yanksterrr@gmail.com>
2025-04-05 12:37:37 +0300

    generated files removed

--- rtl/user_domain/obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /foss/tools/verilator/share/verilator/include/verilated_threads.cpp \
- /foss/tools/verilator/share/verilator/include/verilatedos.h \
- /foss/tools/verilator/share/verilator/include/verilated_threads.h \
- /foss/tools/verilator/share/verilator/include/verilated.h \
- /foss/tools/verilator/share/verilator/include/verilated_config.h \
- /foss/tools/verilator/share/verilator/include/verilated_types.h \
- /foss/tools/verilator/share/verilator/include/verilated_funcs.h
--- rtl/user_domain/obj_dir/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /foss/tools/verilator/share/verilator/include/verilated_timing.cpp \
- /foss/tools/verilator/share/verilator/include/verilated_timing.h \
- /foss/tools/verilator/share/verilator/include/verilated.h \
- /foss/tools/verilator/share/verilator/include/verilatedos.h \
- /foss/tools/verilator/share/verilator/include/verilated_config.h \
- /foss/tools/verilator/share/verilator/include/verilated_types.h \
- /foss/tools/verilator/share/verilator/include/verilated_funcs.h
