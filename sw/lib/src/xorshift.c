d6ceaa3b2ed3fed1beea8c6c55cc0702481c9a4d
Sergii Bidnyi <Sergii_Bidnyi@epam.com>
2025-06-10 22:10:12 +0300

    xorshift api updated

--- sw/lib/src/xorshift.c
+++ sw/lib/src/xorshift.c
@@ -2,12 +2,8 @@
 #include "config.h"
 #include "xorshift.h"
 
-//volatile uint32_t* prng_ptr          = (volatile uint32_t*)USER_PRNG_BASE_ADDR;
-
 uint32_t xorshift32(uint16_t seed) {
-    //seed between 0x000 and 0xFFA, if larger, use 0xFFA
-    seed = seed > (0xFFA) ? 0xFFA : seed; // Ensure seed is within bounds
-    // read uint32_t value by adding the offset to the base address
-    //return *(volatile uint32_t *)(USER_PRNG_BASE_ADDR + seed);
-    return *reg32(USER_PRNG_BASE_ADDR, 0x000);
+    //seed between 0x000 and 0x400, if larger, use seed % 0x400
+    seed = seed > 0x400 ? seed % 0x400 : seed; // Ensure offset is within bounds
+    return *reg32(USER_PRNG_BASE_ADDR, seed * 4);
 }
